c55db918c51f2123c4cc7329d383ce237c711900
Kyeongran <eggking2580@gmail.com>
2024-11-14 23:35:31 +0900

    modify program termination conditions

--- src/lv2/Calculator.java
+++ src/lv2/Calculator.java
@@ -1,7 +1,10 @@
 package lv2;
 
+import java.util.LinkedList;
+
 public class Calculator {
-    public int calculate(int num1, int num2, String operator) {
+    private final LinkedList<Integer> linkedList = new LinkedList<>();
+    public int calculate(int num1, int num2, String operator) { //수정 불가
         int result = 0;
 
         switch (operator) {
@@ -18,6 +21,10 @@ public class Calculator {
                 System.out.println("잘못된 연산자 입니다. 다시 입력해주세요.");
             }
         }
+        linkedList.add(result);
         return result;
+    }
+    public LinkedList<Integer> history() {
+        return linkedList;
     }
 }
--- src/lv2/CalculatorApp.java
+++ src/lv2/CalculatorApp.java
@@ -1,30 +1,34 @@
 package lv2;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class CalculatorApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Calculator calculator = new Calculator();
 
-
         while (true) {
+            System.out.println("계산을 시작하시겠습니까? Yes or No : ");
+            String input = sc.next();
+
+            if (input.equalsIgnoreCase("No")) {
+                break;
+            } else if (!input.equalsIgnoreCase("Yes")) {
+                System.out.println("잘못된 입력입니다. Yes 혹은 No만 입력할 수 있습니다.");
+                continue;
+            }
             System.out.print("숫자를 입력하세요 : ");
             int num1 = sc.nextInt();
 
-            System.out.print("연산자를 입력하세요 (exit 입력 시 종료됩니다) : ");
+            System.out.print("연산자를 입력하세요 : ");
             String operator = sc.next();
-
-            if (operator.equals("exit")) {
-                System.out.println("프로그램을 종료합니다. ");
-                break;
-            }
 
             System.out.print("숫자를 입력하세요 : ");
             int num2 = sc.nextInt();
 
             int result = calculator.calculate(num1, num2, operator); //calculate 함수 호출
             System.out.println(num1 + operator + num2 + " = " + result);
+            System.out.println(calculator.history());
         }
         sc.close();
     }
