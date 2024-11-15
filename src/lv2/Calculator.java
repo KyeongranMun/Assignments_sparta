c916bde5dd4e38952b33e85804086e01925bb8a7
Kyeongran <eggking2580@gmail.com>
2024-11-15 09:40:37 +0900

    lv2 calculator modified : delete the first result when database is fulled

--- src/lv2/Calculator.java
+++ src/lv2/Calculator.java
@@ -14,15 +14,20 @@ public class Calculator {
             case "/" -> {
                 if (num2 == 0) {
                     System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+                    return result; //예외 1 : 0으로 나눌 경우 계산 중단
                 }
                 result = num1 / num2;
             }
             default -> {
                 System.out.println("잘못된 연산자 입니다. 다시 입력해주세요.");
+                return result; // 예외 2 : 잘못된 연산자 입력할 경우 계산 중단
             }
         }
+        if (linkedList.size() >= 5) {
+            linkedList.pollFirst(); //결과 5개가 쌓이면 가장 먼저 들어간 데이터 삭제
+        }
         linkedList.add(result);
-        return result;
+        return result; //계산 결과 추가
     }
     public LinkedList<Integer> history() {
         return linkedList;
--- src/lv2/CalculatorApp.java
+++ src/lv2/CalculatorApp.java
@@ -7,6 +7,9 @@ public class CalculatorApp {
         Scanner sc = new Scanner(System.in);
         Calculator calculator = new Calculator();
 
+        //getter
+        //setter
+
         while (true) {
             System.out.println("계산을 시작하시겠습니까? Yes or No : ");
             String input = sc.next();
@@ -15,7 +18,7 @@ public class CalculatorApp {
                 break;
             } else if (!input.equalsIgnoreCase("Yes")) {
                 System.out.println("잘못된 입력입니다. Yes 혹은 No만 입력할 수 있습니다.");
-                continue;
+                continue; //잘못된 입력일 경우 프로그램 처음부터 다시 실행
             }
             System.out.print("숫자를 입력하세요 : ");
             int num1 = sc.nextInt();
@@ -28,7 +31,7 @@ public class CalculatorApp {
 
             int result = calculator.calculate(num1, num2, operator); //calculate 함수 호출
             System.out.println(num1 + operator + num2 + " = " + result);
-            System.out.println(calculator.history());
+            System.out.println("계산 기록 : " + calculator.history());
         }
         sc.close();
     }
