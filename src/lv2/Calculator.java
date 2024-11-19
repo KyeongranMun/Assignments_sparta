f099f4496644001db4c048a1a0d65237de797abd
Kyeongran <eggking2580@gmail.com>
2024-11-19 21:39:09 +0900

    lv2 calculator modified- operator input  exception

--- src/lv2/Calculator.java
+++ src/lv2/Calculator.java
@@ -1,5 +1,37 @@
 package lv2;
 
-public class Calculator {
+import java.util.LinkedList;
 
+public class Calculator {
+    private final LinkedList<Integer> linkedList = new LinkedList<>();
+
+    public int calculate(int num1, int num2, String operator) {
+        int result=0;
+
+        switch (operator) {
+            case "+" -> result = num1 + num2;
+            case "-" -> result = num1 - num2;
+            case "*" -> result = num1 * num2;
+            case "/" -> {
+                if (num2 == 0) {
+                    System.out.println("0으로 나눌 수 없습니다. 다시 입력하세요.");
+                }
+                result = num1 / num2;
+            }
+            default -> {
+                System.out.println("잘못된 연산자 입니다. ( +, -, *, /) 만 입력할 수 있습니다.");
+            }
+        }
+        addResult(result);
+        return result;
+    }
+    public LinkedList<Integer> getHistory() {
+        return new LinkedList<>(linkedList);
+    }
+    private void addResult(int result) {
+        if (linkedList.size() >= 5) {
+            linkedList.pollFirst();
+        }
+        linkedList.add(result);
+    }
 }
--- src/lv2/CalculatorApp.java
+++ src/lv2/CalculatorApp.java
@@ -1,7 +1,37 @@
 package lv2;
 
+import java.util.Scanner;
+
 public class CalculatorApp {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Calculator calculator = new Calculator();
 
+        while (true) {
+            System.out.print("계산을 진행하시겠습니까? (Yes or No) : ");
+            String input = sc.next();
+
+            if (input.equalsIgnoreCase("no")) {
+                break;
+            } else if (!input.equalsIgnoreCase("yes")) {
+                System.out.print("Yes 또는 No만 입력할 수 있습니다.");
+                continue;
+            }
+
+            System.out.print("첫 번째 숫자를 입력하세요 : ");
+            int num1 = sc.nextInt();
+
+            System.out.print("연산자를 입력하세요 ( +, -, *, /) : ");
+            String operator = sc.next("[+\\-*/]");
+
+            System.out.print("두 번째 숫자를 입력하세요 : ");
+            int num2 = sc.nextInt();
+
+            int result = calculator.calculate(num1, num2, operator);
+
+            System.out.println("계산 결과 : " + num1 + operator + num2 + " = " + result);
+            System.out.println("계산 기록 : " + calculator.getHistory());
+        }
+        sc.close();
     }
 }
