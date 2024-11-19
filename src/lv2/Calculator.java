b896b34cb6e4647a832d08fdc10ba7007d99e44e
Kyeongran <eggking2580@gmail.com>
2024-11-19 20:01:47 +0900

    lv1 , lv2 calculator rework

--- src/lv2/Calculator.java
+++ src/lv2/Calculator.java
@@ -1,38 +1,5 @@
 package lv2;
 
-import java.util.LinkedList;
-
 public class Calculator {
-    private final LinkedList<Integer> linkedList = new LinkedList<>();
 
-    public int calculate(int num1, int num2, String operator) {
-        int result = 0;
-
-        switch (operator) {
-            case "-" -> result = num1 - num2;
-            case "+" -> result = num1 + num2;
-            case "*" -> result = num1 * num2;
-            case "/" -> {
-                if (num2 == 0) {
-                    System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
-                    return result; //예외 1. 0으로 나눌 경우 계산 중단
-                }
-                result = num1 / num2;
-            }
-            default -> System.out.println("잘못된 연산자 입니다. 다시 입력해주세요.");//예외 2. 잘못된 연산자 입력 시 계산 중단
-        }
-        //setter 메서드 추가
-            addResult(result);//계산 결과 linkedList에 저장
-            return result;
-        }
-        //getter 메서드 추가
-        public LinkedList<Integer> getHistory() { //계산 기록 조회
-            return new LinkedList<>(linkedList); //getHistory 메서드에서 new 키워드로 새로운 LinkedList 인스턴스 생성(linkedList를 복사).메인에서 기록 조회만 가능하고 수정은 불가
-        }
-        private void addResult(int result){ //addResult 메서드에 접근하지 못하도록 private 접근제어자 사용
-            if (linkedList.size() >= 5) {
-                linkedList.pollFirst(); //결과 5개가 쌓이면 가장 먼저 들어간 데이터 삭제
-            }
-            linkedList.add(result);
-        }
-    }
+}
--- src/lv2/CalculatorApp.java
+++ src/lv2/CalculatorApp.java
@@ -1,36 +1,7 @@
 package lv2;
 
-import java.util.*;
-
 public class CalculatorApp {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Calculator calculator = new Calculator(); //생성자가 Calculator
 
-        while (true) {
-            System.out.println("계산을 시작하시겠습니까? Yes or No : ");
-            String input = sc.next();
-
-            if (input.equalsIgnoreCase("No")) {
-                break;
-            } else if (!input.equalsIgnoreCase("Yes")) {
-                System.out.println("잘못된 입력입니다. Yes 혹은 No만 입력할 수 있습니다.");
-                continue; //예외 3
-            }
-            System.out.print("숫자를 입력하세요 : ");
-            int num1 = sc.nextInt();
-
-            System.out.print("연산자를 입력하세요 : ");
-            String operator = sc.next();
-
-            System.out.print("숫자를 입력하세요 : ");
-            int num2 = sc.nextInt();
-
-            int result = calculator.calculate(num1, num2, operator); //calculate 함수 호출
-            System.out.println(num1 + operator + num2 + " = " + result);
-            System.out.println("계산 기록 : " + calculator.getHistory());//getHistory 메서드를 사용해 기록 조회
-            sc.close();
-        }
     }
 }
-
